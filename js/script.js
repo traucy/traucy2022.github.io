7949670c6e6eea7d9298e47c5f62fd54060e39c4
Tracy Chen <57275901+traucy@users.noreply.github.com>
2022-01-31 17:37:37 -0800

    Resume update

--- js/script.js
+++ js/script.js
@@ -28,19 +28,6 @@ div.onclick = function() {
   document.getElementById("mobileDiv").style.zIndex = "6";
 }
 
-// var body = document.querySelector("body");
-// var showModal = function (e) {
-//     modal.classList.toggle("hidden");
-//
-//     if (!modal.classList.contains("hidden")) {
-//         // Disable scroll
-//         body.style.overflow = "hidden";
-//     } else {
-//         // Enable scroll
-//         body.style.overflow = "auto";
-//     }
-// };
-
 
 //hide header on scroll
 var prevScrollpos = window.pageYOffset;
